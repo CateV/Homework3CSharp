17fb1768e4df031f12db23edc03012d45a246475
Kate V <katsiarynait@gmail.com>
2023-02-11 13:59:06 +0300

    готово

---
+++ Task1.Palindrom/Program.cs
@@ -0,0 +1,25 @@
+﻿int InputInt(string message)
+{
+    System.Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+int TurnOver(int number)
+{
+    int newNumber = 0;
+    while (number > 0)
+    {
+        newNumber = newNumber * 10 + number % 10;
+        number = number / 10;
+    }
+    return newNumber;
+}
+int number = InputInt("введите число");
+int secondNumber = TurnOver(number);
+
+    if (number == secondNumber)
+        System.Console.WriteLine($"Да, {number}- палиндром");
+    else
+        System.Console.WriteLine($"Нет, {number} не палиндром");
+
+
