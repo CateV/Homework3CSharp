aec0c18040f813ccb2649e5cc4607a46cd8fd8cc
Kate V <katsiarynait@gmail.com>
2023-02-12 13:25:09 +0300

    готово, решение через string

---
+++ Task3.CubesVersion2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт таблицу кубов чисел от 1 до N
+
+int InputInt(string message)
+{
+    System.Console.WriteLine(message);
+    int input = Convert.ToInt32(Console.ReadLine());
+    return input;
+}
+string CubicTable(int n)
+{
+    string output = String.Empty;
+    for (int i = 1; i <= n; i++)
+    {
+        output = output + i * i * i;
+        if (i < n) output = output + ", ";
+    }
+    return output;
+}
+int number = InputInt("введите число больше 0");
+string toPrint = CubicTable(number);
+System.Console.WriteLine(toPrint);
+
+
+
